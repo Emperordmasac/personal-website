81e3b873602f51d9da5c42cb85ad1441fce7e545
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-10-19 00:00:05 +0100

    feat: add work section

--- data/resume-data.tsx
+++ data/resume-data.tsx
@@ -1,5 +1,7 @@
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons"
 
+import { AuditbarLogo, GoCharge, GymFit } from "@/public"
+
 export const RESUME_DATA = {
   name: "Olamilekan Abiola",
   initials: "OA",
@@ -29,5 +31,51 @@ export const RESUME_DATA = {
         icon: LinkedInIcon
       }
     ]
-  }
+  },
+  work: [
+    {
+      company: "Auditbar",
+      link: "https://auditbar.com",
+      badges: ["Remote"],
+      title: "Senior frontend engineer",
+      logo: AuditbarLogo,
+      start: "2023",
+      end: "Present",
+      description:
+        "Spearheaded the design and development of customer-centric features for an e-commerce application, Conceptualized, designed and Implemented functionalities including an inventory and discount system, Designed and implemented the entire storefront frontend from scratch, contributing to a more efficient and user-friendly platform and more."
+    },
+    {
+      company: "GoCharge",
+      link: "https://gocharge.tech/",
+      badges: ["Contract"],
+      title: "Frontend engineer",
+      logo: GoCharge,
+      start: "10/2022",
+      end: "12/2023",
+      description:
+        "Built responsive and scalable web applications using React, Next.js, and Typescript (leveraging MaterialUI framework), Collaborated closely with the backend teams to refine and define API requirements, ensuring efficient communication and seamless integration between frontend and backend systems."
+    },
+    {
+      company: "GymFit",
+      link: "https://auditbar.com",
+      badges: ["Remote"],
+      title: "Software engineer",
+      logo: GymFit,
+      start: "02/2021",
+      end: "11/2022",
+      description:
+        "Designed and implemented solutions from scratch using ReactJS. This involved setting up essential tools such as linting, pre-commit hooks, and pre-push hooks, Recognizing the importance of staying up-to-date with industry trends, I introduced advanced React patterns and best practices to our development workflow."
+    },
+    {
+      company: "Herlabytes",
+      link: "https://www.herlabytes.com/",
+      badges: ["Internship", "Hybrid"],
+      title: "Frontend engineer",
+      logo: AuditbarLogo,
+      start: "09/2019",
+      end: "03/2021",
+      description:
+        "Built responsive and scalable web applications using React, Next.js, and Typescript, Collaborated closely with the backend teams to refine and define API requirements, ensuring efficient communication and seamless integration between frontend and backend systems."
+    }
+  ]
 } as const
---
+++ public/index.ts
@@ -0,0 +1,7 @@
+import GymFit from './gymfit.jpeg'
+import GoCharge from './gocharge.png'
+import AuditbarLogo from './auditbar.png'
+
+export {
+    AuditbarLogo, GoCharge, GymFit
+}
